8754e2d82bc478fbf0e95a00ea34a57bda8e182b
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2022-03-25 11:49:54 +0000

    updated comments

--- index.js
+++ index.js
@@ -9,16 +9,13 @@ const path = require('path');
 const public = path.join(__dirname,'public');
 app.use(express.static(public));
 
-//app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-//app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 const session = require('express-session');
 const auth = require('./auth/auth');
 const passport = require('passport');
 
-app.use(session({ secret: 'dont tell anyone', resave: false,
-saveUninitialized: false }));
+app.use(session({ secret: 'dont tell anyone', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 auth.init(app);
